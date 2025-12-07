0c0988591e18d1d6749f407fec799d799aa2791b
Bhupesh Bashera <bhupeshbashera30@gmail.com>
2025-12-08 02:27:31 +0530

    Fix Speak with Expert button to copy phone number instead of tel: link

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -472,7 +472,7 @@ export default function Index() {
               Start Your Journey
             </Button>
             <Button variant="outline" size="lg" className="text-lg px-8 py-6 glass"
-            onClick={() => window.location.href = "tel:+918630113945"}>
+            onClick={() => handleShowContact('phone')}>
             <Phone className="w-5 h-5 mr-2" />
             Speak with Expert
             </Button>
