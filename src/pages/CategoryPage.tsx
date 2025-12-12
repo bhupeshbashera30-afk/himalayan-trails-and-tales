acd67d66382b9678923e0538a46dc6e1a9c9be5f
Bhupesh Bashera <bhupeshbashera30@gmail.com>
2025-12-12 05:42:09 +0530

    Change grid layout from 3 to 4 columns

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -200,7 +200,7 @@ export default function CategoryPage() {
           </div>
         </motion.div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {destinations.map((destination, index) => (
             <motion.div
               key={destination.id}
