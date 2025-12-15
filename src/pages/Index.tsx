3ad9f6b31b54b15fd73f966b541776a46ae02aa4
Bhupesh Bashera <bhupeshbashera30@gmail.com>
2025-12-16 05:09:34 +0530

    twerk

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -554,7 +554,7 @@ const copyToClipboard = () => {
 
         {/* Floating elements */}
         <div className="absolute top-20 left-10 w-20 h-20 bg-primary/20 rounded-full blur-xl float" style={{ animationDelay: '0s' }} />
-        <div className="absolute bottom-20 right-10 w-32 h-32 bg-accent/20 rounded-full blur-xl float" style={{ animationDelay: '2s' }} />
+      
         <div className="absolute top-1/2 left-1/4 w-16 h-16 bg-primary-glow/20 rounded-full blur-xl float" style={{ animationDelay: '4s' }} />
       </section>
 
