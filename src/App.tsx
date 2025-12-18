8483679d3b87f726f10ca4365817d4ce37222145
Bhupesh Bashera <bhupeshbashera30@gmail.com>
2025-12-19 01:30:02 +0530

    Add Packages route to App.tsx routing configuration

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Index from './pages/Index';
 import CategoryPage from './pages/CategoryPage';
 import NotFound from './pages/NotFound';
+import Packages from './pages/Packages';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Route path="/" element={<Index />} />
         <Route path="/category/:slug" element={<CategoryPage />} />
         <Route path="/booking/:id" element={<Index />} />
+                      <Route path="/packages" element={<Packages />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
