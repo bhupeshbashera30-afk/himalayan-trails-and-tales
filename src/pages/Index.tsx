bb4b0370e4c6abcc19e00ae52cebf35665ed033d
Bhupesh Bashera <bhupeshbashera30@gmail.com>
2025-12-27 13:37:10 +0530

    Text Changes to be committed:

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -473,9 +473,8 @@ const copyToClipboard = () => {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.2 }}
           >
-            Discover the
+            Discover Pahadi Spirit
             <br />
-            <span className="float">Pahadi Spirit</span>
           </motion.h1>
           
           <motion.p 
